bc20a624781ce456102c8e464c4d0339399a3283
Stefanie Loh <stefkloh@gmail.com>
2018-04-03 16:03:50 -0700

    Added functions to word.js

--- letter.js
+++ letter.js
@@ -1,42 +1,46 @@
 // This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 
-function Letter() {
+var Letter = function(correctLetter) {
 
     //   * A string value to store the underlying character for the letter
-    this.correctLetter = 'a';
+    this.correctLetter = correctLetter;
+    console.log(this.correctLetter + ' is the correct letter.')
 
     //   * A boolean value that stores whether that letter has been guessed yet
-    this.guessed = false;
+    this.show = false;
 
-    //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-    this.isGuessed = function() {
-        if (this.guessed === true) {
-            console.log(this.correctLetter);
-        }
-        else {
-            console.log('_');
-        }
+}
+
+//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+Letter.prototype.print = function() {
+    if (this.show === true) {
+        console.log(this.correctLetter);
     }
-
-    //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-    this.isCorrect = function(character) {
-        if (character === this.correctLetter) {
-            console.log(character + ' is correct');
-            this.guessed = true;
-        }
-        else {
-            console.log(character + ' is incorrect');
-        }
+    else {
+        console.log('_');
     }
 }
 
-var letters = {};
+//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+Letter.prototype.compareToGuess = function(guess) {
+    if (guess === this.correctLetter) {
+        console.log('Guess "' + guess + '" is correct');
+        this.show = true;
+    }
+    else {
+        console.log('Guess "' + guess + '" is incorrect');
+    }
+}
 
-letters.a = new Letter();
+module.exports = Letter;
 
-// var letter = process.argv[2];
 
-// letters[letter];
 
-letters.a.isCorrect('a');
-letters.a.isGuessed();
+// Testing
+var a = new Letter('b');
+a.compareToGuess('a');
+a.print();
+
+var b = new Letter('b');
+b.compareToGuess('b');
+b.print();
--- word.js
+++ word.js
@@ -1,8 +1,33 @@
+var Letter = require('/.letter');
+
 // Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 
+var Word = function(word) {
 
-//   * An array of `new` Letter objects representing the letters of the underlying word
+    this.word = word;
+
+    //   * An array of `new` Letter objects representing the letters of the underlying word
+    this.newLetters = [];
+
+}
 
 //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+Word.prototype.toString = function() {
+
+    var output = " ";
+    for(var i=0; i<this.newLetters.length; i++) {
+        output += this.newLetters[i].print();
+    }
+    return output;
+
+}
 
 //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+Work.prototype.checkGuess = function(guess) {
+
+    for(var i=0; i<this.newLetters.length; i++) {
+        this.newLetters[i].compareToGuess(guess);
+    }
+}
+
+module.exports = Word;
